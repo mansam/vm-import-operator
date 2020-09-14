8cf920990076389003ba7d921c68ecb9547fcea8
Sam Lucidi <slucidi@redhat.com>
2020-09-14 13:44:13 -0400

    Update vSphere mapper to use actual VDDK datasource
    Signed-off-by: Sam Lucidi <slucidi@redhat.com>

--- pkg/providers/vmware/mapper/mapper.go
+++ pkg/providers/vmware/mapper/mapper.go
@@ -16,7 +16,7 @@ import (
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kubevirtv1 "kubevirt.io/client-go/api/v1"
-	cdiv1 "kubevirt.io/containerized-data-importer/pkg/apis/core/v1alpha1"
+	cdiv1 "kubevirt.io/containerized-data-importer/pkg/apis/core/v1beta1"
 )
 
 const (
@@ -252,7 +252,13 @@ func (r *VmwareMapper) MapDataVolumes(targetVMName *string) (map[string]cdiv1.Da
 			},
 			Spec: cdiv1.DataVolumeSpec{
 				Source: cdiv1.DataVolumeSource{
-					Blank: &cdiv1.DataVolumeBlankImage{},
+					VDDK: &cdiv1.DataVolumeSourceVDDK{
+						URL:         r.credentials.URL,
+						UUID:        r.vmProperties.Config.Uuid,
+						BackingFile: disk.backingFileName,
+						Thumbprint:  r.credentials.Thumbprint,
+						SecretRef:   r.credentials.SecretName,
+					},
 				},
 				PVC: &corev1.PersistentVolumeClaimSpec{
 					AccessModes: []corev1.PersistentVolumeAccessMode{
